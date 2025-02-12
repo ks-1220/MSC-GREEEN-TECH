aec9112007244da963b01b610cb2952a98aca5d4
ks-1220 <yeahkriti30@gmail.com>
2025-02-12 13:43:14 +0530

    Update sdi.py

--- sdi.py
+++ sdi.py
@@ -80,7 +80,7 @@ st.title("🌊 Effluent Prediction Dashboard")
 
 # Sidebar Inputs
 st.sidebar.header("User Inputs")
-industry = st.sidebar.selectbox("Select Industry", df['Industry']
+industry = st.sidebar.selectbox("Select Industry", df['Industry'].astype(str).unique())
 year = st.sidebar.slider("Select Year", 2025, 2030, 2027)
 
 # Fetch prediction for selected industry and year
